7849ee42630799fc51d061992a6d551415cba042
Mahnoor Maqsood <mahnoor.maqsood@gmail.com>
2018-01-09 18:04:18 -0500

    finished basic tutorial

--- PropertyFinder/SearchPage.js
+++ PropertyFinder/SearchPage.js
@@ -10,6 +10,25 @@ import {
   ActivityIndicator,
   Image,
 } from 'react-native';
+import SearchResults from './SearchResults';
+
+function urlForQueryAndPage(key, value, pageNumber) {
+  const data = {
+      country: 'uk',
+      pretty: '1',
+      encoding: 'json',
+      listing_type: 'buy',
+      action: 'search_listings',
+      page: pageNumber,
+  };
+  data[key] = value;
+
+  const querystring = Object.keys(data)
+    .map(key => key + '=' + encodeURIComponent(data[key]))
+    .join('&');
+
+  return 'https://api.nestoria.co.uk/api?' + querystring;
+}
 
 export default class SearchPage extends Component<{}> {
   constructor(props) {
@@ -17,12 +36,48 @@ export default class SearchPage extends Component<{}> {
     this.state = {
       searchString: 'london',
       isLoading: false,
+      message: '',
     };
   }
+
   _onSearchTextChanged = (event) => {
-    this.setState({ searchString: event.nativeEvent.text});
-  }
+    this.setState({ searchString: event.nativeEvent.text });
+  };
+
+  _executeQuery = (query) => {
+    console.log(query);
+    this.setState({ isLoading: true });
+    fetch(query)
+      .then(response => response.json())
+      .then(json => this._handleResponse(json.response))
+      .catch(error =>
+      	this.setState({
+      	  isLoading: false,
+      	  message: 'Something bad happened ' + error
+      }));
+  };
+
+  _handleResponse = (response) => {
+    this.setState({ isLoading: false , message: '' });
+    if (response.application_response_code.substr(0, 1) === '1') {
+      this.props.navigator.push({
+      	title: 'Results',
+      	component: SearchResults,
+      	passProps: {listings: response.listings}
+      });
+    } else {
+      this.setState({ message: 'Location not recognized; please try again.'});
+    }
+  };
+
+  _onSearchPressed = () => {
+    const query = urlForQueryAndPage('place_name', this.state.searchString, 1);
+    this._executeQuery(query);
+  };
+
   render() {
+  	const spinner = this.state.isLoading ?
+      <ActivityIndicator size='large'/> : null;
     return (
       <View style={styles.container}>
         <Text style={styles.description}>
@@ -38,12 +93,14 @@ export default class SearchPage extends Component<{}> {
             onChange={this._onSearchTextChanged}
             placeholder='Search via name or postcode'/>
           <Button
-            onPress={() => {}}
+            onPress={this._onSearchPressed}
             color='#48BBEC'
             title='Go'
           />
         </View>
         <Image source={require('./Resources/house.png')} style={styles.image}/>
+        {spinner}
+        <Text style={styles.description}>{this.state.message}</Text>
       </View>
     );
   }
@@ -52,7 +109,7 @@ export default class SearchPage extends Component<{}> {
 const styles = StyleSheet.create({
   description: {
     marginBottom: 20,
-    fontSize : 18,
+    fontSize: 18,
     textAlign: 'center',
     color: '#656565'
   },
